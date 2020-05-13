33b7549839f10ab536e5514a9ec9188d5dc6c987
Mohamed Elshaarawy <elshaarawy200825@hotmail.com>
2020-05-14 00:41:13 +0200

    fix: curves now consider tangent points in calculations, also start and end points of the polygon is connected

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
-const NONE = 1
-const CHAMFER = 2
-const FILLET = 3
-let points = [
-    { x: 100, y: 400, r: 10, type: NONE },
-    { x: 100, y: 100, r: 80, type: FILLET },
-    { x: 400, y: 100, r: 80, type: CHAMFER },
-    { x: 400, y: 400, r: 70, type: FILLET },
-    { x: 600, y: 400, r: 100, type: NONE },
-    { x: 800, y: 200, r: 50, type: CHAMFER },
-    { x: 1000, y: 400, r: 50, type: CHAMFER }];
+const NONE = 'none'
+const CHAMFER = 'chamfer'
+const FILLET = 'fillet'
+
 
 function formPath(points) {
 
@@ -17,23 +10,27 @@ function formPath(points) {
 
         const currentPoint = points[i];
 
-        if (i == 0) {
-            result = result.concat(formStartPoint(currentPoint), " ");
-            continue;
+        if (![NONE, CHAMFER, FILLET].includes(currentPoint.type)) {
+            throw `The current point type ${currentPoint.type} is not handled`
         }
 
-        if (i == points.length - 1) {
-            result = result.concat(formLine(currentPoint))
-            continue;
-        }
+        // if (i == points.length - 1) {
+        //     result = result.concat(formLine(currentPoint))
+        //     continue;
+        // }
 
         if (currentPoint.type == NONE) {
             result = result.concat(formLine(currentPoint), " ")
             continue;
         }
 
-        const prePoint = points[i - 1]
-        const nextPoint = points[i + 1]
+        const prePoint = i == 0 ? points[points.length - 1] : points[i - 1]
+        const nextPoint = i < points.length - 1 ? points[i + 1] : points[0]
+
+        if (i == 0) {
+            result = result.concat(formInitPoint(prePoint, currentPoint, nextPoint), " ")
+            continue;
+        }
 
         if (!isValidChamferFillet(prePoint, currentPoint, nextPoint)) {
             result = result.concat(formLine(currentPoint), " ")
@@ -49,10 +46,10 @@ function formPath(points) {
             result = result.concat(formFillet(prePoint, currentPoint, nextPoint), " ")
             continue
         }
-
-        throw `The current point type ${currentPoint.type} is not handled`
     }
-    return result;
+    console.log(result);
+
+    return result.concat(" Z");
 }
 
 function formStartPoint(point) {
@@ -64,27 +61,37 @@ function formLine(point) {
 }
 
 function formatArc(p, radius, sweep) {
-    return `A ${radius} ${radius} 0 0 ${sweep} ${p.x} ${p.y}`
+    return `A${radius} ${radius} 0 0 ${sweep} ${p.x} ${p.y}`
 }
 
 function isValidChamferFillet(prePoint, currentPoint, nextPoint) {
+    const tangentLength = tangentLinesLength(prePoint, currentPoint, nextPoint)
+    return tangentLength
+}
 
-    var preTotalradius = prePoint.r + currentPoint.r
-    var nextTotalradius = currentPoint.r + nextPoint.r;
+function formInitPoint(prePoint, currentPoint, nextPoint) {
+    if (currentPoint.type == NONE) {
+        return formStartPoint(currentPoint)
+    }
+    const touchPoints = touchCirclePoints(prePoint, currentPoint, nextPoint)
+    const startPoint = touchPoints[0]
+    const endPoint = touchPoints[1]
 
-    const preTotalDistance = distanceBetweenTwoPoints(prePoint, currentPoint)
-    const nextTotalDistance = distanceBetweenTwoPoints(currentPoint, nextPoint)
+    if (currentPoint.type == CHAMFER) {
+        return formStartPoint(startPoint).concat(" ", formLine(endPoint))
+    }
 
-    const preRatio = (preTotalDistance - preTotalradius) / preTotalDistance
-    const nextRatio = (nextTotalDistance - nextTotalradius) / nextTotalDistance
-
-    return preRatio >= 0 && preRatio <= 1 && nextRatio >= 0 && nextRatio <= 1
+    if (currentPoint.type == FILLET) {
+        const isReversed = isArcReversed(prePoint, currentPoint, nextPoint) > 0 ? 0 : 1
+        return formStartPoint(startPoint).concat(" ", formatArc(endPoint, currentPoint.r, isReversed))
+    }
 
 }
 
 function formChamfer(prePoint, currentPoint, nextPoint) {
-    const startPoint = pointBetweenTwoPoints(prePoint, currentPoint, currentPoint.r)
-    const endPoint = pointBetweenTwoPoints(nextPoint, currentPoint, currentPoint.r)
+    const touchPoints = touchCirclePoints(prePoint, currentPoint, nextPoint)
+    const startPoint = touchPoints[0]
+    const endPoint = touchPoints[1]
     return formLine(startPoint).concat(" ", formLine(endPoint))
 }
 
@@ -96,25 +103,37 @@ function formFillet(prePoint, currentPoint, nextPoint) {
         return formLine(currentPoint)
     }
 
-    const startPoint = pointBetweenTwoPoints(prePoint, currentPoint, currentPoint.r)
-    const endPoint = pointBetweenTwoPoints(nextPoint, currentPoint, currentPoint.r)
-    const isReversed = angle < 180 ? 0 : 1
+    const touchPoints = touchCirclePoints(prePoint, currentPoint, nextPoint)
+    const startPoint = touchPoints[0]
+    const endPoint = touchPoints[1]
+
+    const isReversed = isArcReversed(prePoint, currentPoint, nextPoint) > 0 ? 0 : 1
 
     return formLine(startPoint).concat(" ", formatArc(endPoint, currentPoint.r, isReversed))
 }
 
+function isArcReversed(prePoint, currentPoint, nextPoint) {
+    if (currentPoint.r == 30) {
+        console.log(prePoint.x - currentPoint.x > 0);
+        console.log(currentPoint.y - prePoint.y > 0);
+        console.log(currentPoint.x - nextPoint.x > 0);
+
+    }
+
+
+    return prePoint.x - currentPoint.x > 0 || currentPoint.y - prePoint.y > 0 || currentPoint.x - nextPoint.x > 0
+}
+
 function angleBetweenTwoLines(line1Start, intersect, line2End) {
-    let radian = Math.atan2(intersect.y - line2End.y, intersect.x - line2End.x) - Math.atan2(intersect.y - line1Start.y, intersect.x - line1Start.x);
+    const angle = radianAngle(line1Start, intersect, line2End) * (180 / Math.PI)
+    return angle;
+}
 
-    while (radian > Math.PI * 2) {
-        radian = radian - (Math.PI * 2)
-    }
-
-    while (radian < 0) {
-        radian = radian + (Math.PI * 2)
-    }
-
-    return Math.round(radian * (180 / Math.PI))
+function radianAngle(A, B, C) {
+    var AB = Math.sqrt(Math.pow(B.x - A.x, 2) + Math.pow(B.y - A.y, 2));
+    var BC = Math.sqrt(Math.pow(B.x - C.x, 2) + Math.pow(B.y - C.y, 2));
+    var AC = Math.sqrt(Math.pow(C.x - A.x, 2) + Math.pow(C.y - A.y, 2));
+    return Math.acos((BC * BC + AB * AB - AC * AC) / (2 * BC * AB));
 }
 
 function distanceBetweenTwoPoints(p1, p2) {
@@ -125,10 +144,64 @@ function pointBetweenTwoPoints(p1, p2, d) {
     const distance = distanceBetweenTwoPoints(p1, p2)
     const newDistance = distance - d
     const ratio = newDistance / distance
-
     const newX = ((1 - ratio) * p1.x) + (ratio * p2.x)
     const newY = ((1 - ratio) * p1.y) + (ratio * p2.y)
-    return { x: newX, y: newY, ratio: ratio }
+    return { x: newX, y: newY }
 }
 
-document.getElementById("pathId").setAttribute('d', formPath(points))
+function tangentLinesLength(line1Start, intersect, line2End) {
+    const angle = radianAngle(line1Start, intersect, line2End)
+    if (angle == 0 || angle == Math.PI) {
+        return 0
+    }
+    return intersect.r / Math.abs(Math.tan(angle / 2))
+}
+
+function touchCirclePoints(line1Start, intersect, line2End) {
+    const tangentLength = tangentLinesLength(line1Start, intersect, line2End);
+    return [
+        pointBetweenTwoPoints(line1Start, intersect, tangentLength),
+        pointBetweenTwoPoints(line2End, intersect, tangentLength)
+    ]
+}
+
+let points = [
+    { x: 100, y: 400, r: 30, type: FILLET },
+    { x: 100, y: 100, r: 10, type: FILLET },
+    { x: 400, y: 100, r: 20, type: FILLET },
+    { x: 400, y: 300, r: 100, type: FILLET },
+    { x: 600, y: 400, r: 40, type: FILLET },
+    { x: 700, y: 200, r: 50, type: FILLET },
+    { x: 1000, y: 400, r: 30, type: FILLET },
+];
+
+let points1 = [{ x: 800, y: 200, r: 100, type: FILLET },
+{ x: 800, y: 800, r: 25, type: FILLET },
+{ x: 850, y: 250, r: 100, type: FILLET }]
+
+let points2 = [
+    { x: 100, y: 300, r: 10, type: FILLET },
+    { x: 0, y: 0, r: 50, type: FILLET },
+    { x: 300, y: 100, r: 80, type: FILLET }]
+
+let points3 = [
+    { x: 0, y: 100, r: 0, type: FILLET },
+    { x: 0, y: 200, r: 20, type: FILLET },
+    { x: 100, y: 20, r: 10, type: FILLET },
+    { x: 100, y: 0, r: 50, type: FILLET },]
+
+
+let points4 = [{ x: 0, y: 50, r: 10, type: 'fillet' },
+{ x: 0, y: 120, r: 50, type: FILLET },
+{ x: 100, y: 20, r: 10, type: FILLET },
+{ x: 100, y: 0, r: 50, type: FILLET },]
+
+
+
+const runPoints = points
+document.getElementById("pathId").setAttribute('d', formPath(runPoints))
+// document.getElementById("helperPath").setAttribute('d', formPath(runPoints.map((i) => {
+//     i.type = NONE
+//     return i
+// })))
+// document.getElementById("pathId").setAttribute('d', "M200,300 Q400,50 600,300")
